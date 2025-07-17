2a67a34784d35404dbe185b143af22cae3c7aeaf
perminand <perminand@gmail.com>
2025-07-17 09:25:16 +0300

    feat: Попытка задеплоить

---
+++ src/main/resources/db/changelog/data/V002_set_company_roles.sql
@@ -0,0 +1,30 @@
+-- Установка ролей для компаний в начальных данных
+-- Компания 1 - Заказчик
+UPDATE companies SET role = 'CUSTOMER' WHERE inn = '7700000001';
+
+-- Компания 2 - Поставщик  
+UPDATE companies SET role = 'SUPPLIER' WHERE inn = '7700000002';
+
+-- Компания 3 - Заказчик
+UPDATE companies SET role = 'CUSTOMER' WHERE inn = '7700000003';
+
+-- Компания 4 - Поставщик
+UPDATE companies SET role = 'SUPPLIER' WHERE inn = '7700000004';
+
+-- Компания 5 - Заказчик
+UPDATE companies SET role = 'CUSTOMER' WHERE inn = '7700000005';
+
+-- Компания 6 - Заказчик
+UPDATE companies SET role = 'CUSTOMER' WHERE inn = '7700000006';
+
+-- Компания 7 - Поставщик
+UPDATE companies SET role = 'SUPPLIER' WHERE inn = '7700000007';
+
+-- Компания 8 - Заказчик
+UPDATE companies SET role = 'CUSTOMER' WHERE inn = '7700000008';
+
+-- Компания 9 - Заказчик
+UPDATE companies SET role = 'CUSTOMER' WHERE inn = '7700000009';
+
+-- Компания 10 - Заказчик
+UPDATE companies SET role = 'CUSTOMER' WHERE inn = '7700000010'; 
